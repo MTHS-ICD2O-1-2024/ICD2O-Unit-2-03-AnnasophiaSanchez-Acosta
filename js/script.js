f957619cd86e2baec2989c5d30b0402b95f3d152
default <annasophia.sanchez-acosta@stu.ocsb.ca>
2025-03-23 19:56:42 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mr. Coxall updated by Annasophia
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
 
 /**
  * Displays an alert with the message "Hello, World!"
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const streetNumber = parseInt(document.getElementById("street-number").value)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your adress is: " + streetName + ", " + streetNumber + "."
 }
